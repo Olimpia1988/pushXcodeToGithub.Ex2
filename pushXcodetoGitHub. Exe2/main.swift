0667cbc68dacd675ee38e5d36dd95d6a6e3c22bd
Olimpia <olimpiacompean@prusuit.org>
2018-10-05 16:45:17 -0400

    First commit

---
+++ pushXcodetoGitHub. Exe2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  pushXcodetoGitHub. Exe2
+//
+//  Created by Olimpia on 10/5/18.
+//  Copyright © 2018 Olimpia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
